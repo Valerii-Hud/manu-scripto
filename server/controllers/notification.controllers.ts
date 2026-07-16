58f86e25032893170721d6ca2e2acd12223c2ca1
Valerii Hud <connect.valerii@gmail.com>
2026-07-16 14:54:17 +0200

    feat: delete notification by id

--- server/controllers/notification.controllers.ts
+++ server/controllers/notification.controllers.ts
@@ -1,8 +1,8 @@
 import errorHandler from "../lib/utils/errorHandler.lib";
-import User from "../models/user.model";
 import Notification from "../models/notification.model";
 import type { AuthRequest } from "../types/interfaces.types";
 import type { Response } from "express";
+import User from "../models/user.model";
 
 export const getNotifications = async (req: AuthRequest, res: Response) => {
   try {
@@ -24,6 +24,48 @@ export const getNotifications = async (req: AuthRequest, res: Response) => {
 
 export const deleteNotifications = async (req: AuthRequest, res: Response) => {
   try {
+    const userId = req.user?._id;
+
+    await Notification.deleteMany({ to: userId });
+
+    return res
+      .status(200)
+      .json({ message: "Notifications deleted successfully" });
+  } catch (error) {
+    errorHandler(res, error);
+  }
+};
+
+export const deleteNotificationById = async (
+  req: AuthRequest,
+  res: Response,
+) => {
+  try {
+    const notificationId = req.params;
+    const userId = req.user?._id;
+
+    if (!userId)
+      return res
+        .status(401)
+        .json({ error: "Unathorized Please Provide userId" });
+
+    const user = await User.findById(userId);
+    const notification = await Notification.findById(notificationId);
+
+    if (!user) return res.status(404).json({ error: "User Not Found" });
+    if (!notification)
+      return res.status(404).json({ error: "Notification Not Found" });
+
+    if (
+      notification.to.toString() !== userId.toString() &&
+      user.userType === "default"
+    ) {
+      await Notification.findByIdAndDelete(notificationId);
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Notification delelted successfully" });
   } catch (error) {
     errorHandler(res, error);
   }
