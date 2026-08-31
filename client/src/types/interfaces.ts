2184db60d388e01dfe9084912b06fcc5e34d1bd8
Valerii Hud <support@valerii-hud.com>
2026-08-31 20:32:34 +0200

    feat(client): types updated

--- client/src/types/interfaces.ts
+++ client/src/types/interfaces.ts
@@ -1,46 +1,112 @@
-export interface IPost {
+export interface MongoDbBase {
   _id: string;
+  createdAt: string;
+  updatedAt: string;
+}
+export interface IPost extends MongoDbBase {
   user: IUser;
   text: string;
   image?: string;
   views?: number;
-  likes?: Id[];
+  likes?: string[];
   comments: IPostComment[];
   tags?: string[];
-  createdAt: string;
 }
 
-export interface IPostComment {
+export interface IPostComment extends MongoDbBase {
   text: string;
-  _id: Id;
   user: IUser;
-  createdAt: string;
 }
 
-export interface INotification {
-  _id: Id;
+export interface INotification extends MongoDbBase {
   from: IUser;
   to: IUser;
   type: string;
   isRead: boolean;
-  createdAt: string;
-  updatedAt: string;
 }
 
-export type Id = string;
-
-export interface IUser {
-  _id: Id;
+export interface UserBaseData {
   userName: string;
-  fullName?: string | null;
-  phoneNumber?: string;
   email?: string;
-  followers?: string[];
-  following?: string[];
-  likedPosts: string[];
   profileImage?: string;
   coverImage?: string;
   bio?: string;
   link?: string;
+}
+export interface IUser extends MongoDbBase, UserBaseData {
+  followers?: string[];
+  following?: string[];
+  likedPosts: string[];
   isHidden?: boolean;
+}
+
+export interface IProfileUpdateData extends UserBaseData {
+  newPassword?: string;
+  currentPassword?: string;
+}
+
+export enum HttpMethod {
+  GET = 'get',
+  POST = 'post',
+  PUT = 'put',
+  DELETE = 'delete',
+}
+export interface ISearchData {
+  searchString: string;
+}
+
+export interface AuthDataBase {
+  userName: string;
+  password: string;
+}
+export interface ISignupData extends AuthDataBase {
+  email: string;
+}
+
+export interface ICreatePostData {
+  image?: string;
+  text?: string;
+  isHidden?: boolean;
+}
+
+export interface IPostCounterTypeData {
+  type: string;
+}
+
+export interface IPostCommentData {
+  isHidden: boolean;
+  text: string;
+}
+
+export type ApiData =
+  | ISignupData
+  | AuthDataBase
+  | ICreatePostData
+  | IPostCounterTypeData
+  | IProfileUpdateData
+  | ISearchData
+  | IPostCommentData;
+
+type StaticEndpoint =
+  | '/api/v1/auth/signup'
+  | '/api/v1/auth/login'
+  | '/api/v1/auth/logout'
+  | '/api/v1/auth/check'
+  | '/api/v1/search';
+
+type DynamicEndpoint =
+  | `/api/v1/users/${string}`
+  | `/api/v1/verify/${string}`
+  | `/api/v1/reports/${string}`
+  | `/api/v1/posts/${string}`
+  | `/api/v1/notifications/${string}`;
+
+export type Endpoint = StaticEndpoint | DynamicEndpoint;
+export interface Api {
+  endpoint: Endpoint;
+  method?: HttpMethod;
+  data?: ApiData;
+  successMessage?: string;
+  errorMessage?: string;
+  showError?: boolean;
 }
